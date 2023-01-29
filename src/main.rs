20264af5a3a0cfb451c643297f6c6ab85951d453
James Courtois <jcourtois@gmail.com>
2023-01-29 12:11:17 -0500

    refactor to use let if for brevity

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@ fn main() {
     println!("Hello, world!");
 }
 
+// Function that returns the indexes of two numbers that sum to a target value
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new();
     for (index, element) in nums.iter().enumerate() {
-        let expected = target - element;
-        match map.get(&expected) {
-            Some(&other_index) => {
-                return vec![other_index as i32, index as i32];
-            }
-            None => {
-                map.insert(element, index);
-            }
+        let pair = map.get(&(target - element));
+        if let Some(&pair_index) = pair {
+            return vec![pair_index as i32, index as i32];
+        } else {
+            map.insert(element, index);
         }
     }
     unreachable!()
@@ -26,16 +24,16 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(two_sum(vec![2,7,11,15], 9), vec![0,1])
+        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1])
     }
 
     #[test]
     fn case2() {
-        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1,2])
+        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2])
     }
 
     #[test]
     fn case3() {
-        assert_eq!(two_sum(vec![3,3], 6), vec![0, 1])
+        assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1])
     }
 }
